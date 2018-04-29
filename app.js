ed560dfde14b73ae5437fbf9a19d5b3cc80b57a9
CYH16 <h5s12s20s@yahoo.com.tw>
2018-04-29 12:19:40 +0800

    reinstall ID/password

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-    appId: process.env.MICROSOFT_APP_ID,
-    appPassword: process.env.MICROSOFT_APP_PASSWORD
+    appId: "a04f837e-479e-44a4-87b7-ca5394baf1ef",
+    appPassword: "3}@{it/#]OnC_paL"
 });
 
 // Listen for messages from users 
